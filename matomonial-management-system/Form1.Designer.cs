75e7fdca159c8c3640d4110305687c83db831248
Mohammad Nur <mdnur701@gmail.com>
2023-04-16 13:00:11 +0600

    Add project files.

---
+++ matomonial-management-system/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace matomonial_management_system
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.PasswordLable = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(370, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailLabel.Location = new System.Drawing.Point(264, 138);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(41, 16);
+            this.EmailLabel.TabIndex = 1;
+            this.EmailLabel.Text = "Email";
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(267, 157);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(267, 20);
+            this.emailBox.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(268, 225);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(267, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // PasswordLable
+            // 
+            this.PasswordLable.AutoSize = true;
+            this.PasswordLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordLable.Location = new System.Drawing.Point(265, 206);
+            this.PasswordLable.Name = "PasswordLable";
+            this.PasswordLable.Size = new System.Drawing.Size(67, 16);
+            this.PasswordLable.TabIndex = 3;
+            this.PasswordLable.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(268, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Sign In";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.PasswordLable);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label PasswordLable;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
